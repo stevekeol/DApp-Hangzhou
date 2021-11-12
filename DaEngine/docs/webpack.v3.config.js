b2d42fa8fcc999c648464449781735395074f22f
stevekeol <stevekeol.x@gmail.com>
2021-11-12 18:39:06 +0800

    :construction: webpack v5 适配中

--- DaEngine/docs/webpack.v3.config.js
+++ DaEngine/docs/webpack.v3.config.js
@@ -3,27 +3,17 @@ const path = require('path')
 
 // 打包文件分析工具：用于后期文件包的优化
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-// 抽离CSS样式，防止将CSS打包在js中引起页面样式加载错乱
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-
-// webpack打包的文件将要存放的位置，本文件中的相对路径都是相对于它
 const DIST_PATH = './lib/template/assets/script'
 const isProd = process.env.NODE_ENV === 'production'
-const isWatched = process.env.WATCH === 'true'
 // const showAnalysis = process.env.ANA === 'true'
-
-// // 将 css 从文本中提取出来，参数为资源存放的位置(此处是lib/template/assets/css/DaEngine.min.css，因为此时的路径是DIST_PATH)
-let plugins = [
-  new MiniCssExtractPlugin({
-    filename: '../css/weweb.min.css'
-  })
-]
-
+const watch = process.env.WATCH === 'true'
+// 将 css 从文本中提取出来，参数为资源存放的位置
+let plugins = [new ExtractTextPlugin('../css/weweb.min.css')]
 // if (showAnalysis) {
 //   plugins = plugins.concat([new BundleAnalyzerPlugin()])
 // }
-
 if (isProd) {
   plugins = plugins.concat([
     new webpack.DefinePlugin({
@@ -57,9 +47,8 @@ function getSourcePath (rPath) {
 }
 
 module.exports = {
-  mode: 'production',
   entry: {
-    DaEngine: getSourcePath('index.ts')
+    weweb: getSourcePath('index.js')
   },
   output: {
     filename: '[name].js',
@@ -67,45 +56,46 @@ module.exports = {
     chunkFilename: '[name].wd.chunk.js',
     path: getPath(DIST_PATH)
   },
-  watch: isWatched,
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  },
+  watch: watch,
   module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
+    loaders: [
       {
         test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['env', 'stage-0']
+        }
       },
       {
-        test: /\.html/i,
-        use: 'html-loader'
+        test: /\.html/,
+        loader: 'html-loader'
       },
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: {
+            loader: 'css-loader',
+            options: { minimize: true }
+          }
+        })
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        use: [
-          'file-loader'
+        loaders: [
           // 注意此处 outputPath 为输出结果的地址
-          // 'file-loader?name=[name].[ext]&publicPath=&outputPath=../images/'
+          'file-loader?name=[name].[ext]&publicPath=&outputPath=../images/'
+          // 'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
         ]
       },
       {
         test: /\.et/,
-        use: 'ei-loader'
+        loader: 'ei-loader'
       },
       {
         test: /\.json$/,
-        use: 'json'
+        loader: 'json'
       }
     ]
   },
