bcdec2959da7ba20ddc40dfef52e4fcf35e4355b
stevekeol <stevekeol.x@gmail.com>
2021-11-13 23:11:34 +0800

    :memo: 梳理了runtime和view在整个编译运行中的工程层面的逻辑

--- DaEngine/dev.sh
+++ DaEngine/dev.sh
@@ -6,7 +6,7 @@ app='demos/demo_tencent'
 # 加了 dev 参数就不编译 framwork
 if [ "$1" != "dev" ]; then
   # 不压缩代码
-  npm run dev:nowatch
+  npm run dev:nowatch # 根据webpack配置文件，会将src/index.ts中引入的所有文件编译后写入lib/templates/assets/scripts下面
 fi
 
-./bin/DaEngine -b $app
+./bin/DaEngine -b $app # 2 将demo_dapp按内部规则编译后，结合lib/templates/assets/scripts中的js文件，生成wewebTemp的最终文件。
